0345f81b842d6e191ea8e92cc3ee7688d42bfc34
BuildTools <aelozweb@gmail.com>
2023-12-23 18:43:54 +0100

    improved shoot design

--- src/entity/player_attack.rs
+++ src/entity/player_attack.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use crate::states::game::OnGameScreen;
 
-#[derive(Component)]
+#[derive(Component, Clone, Copy)]
 pub struct Projectil {
     pub(crate) size: f32,
     pub(crate) position: Vec3,
@@ -26,11 +26,22 @@ pub fn spwan_projectil(
     position: Vec3
 ) {
     let projectil = Projectil::new(position);
+
     commands.spawn((
         MaterialMesh2dBundle {
-           mesh: meshes.add(shape::Quad::new(Vec2::new(projectil.size*2.0, projectil.size/2.0)).into()).into(),
+           mesh: meshes.add(shape::Quad::new(Vec2::new(projectil.size*1.5, projectil.size/2.0)).into()).into(),
             material: materials.add(ColorMaterial::from(Color::rgb(2.45, 1.85, 0.66))),
             transform: Transform::from_translation(projectil.position),
+            ..default()
+        },
+        projectil,
+        OnGameScreen
+    ));
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(projectil.size/1.5).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::rgb(2.45, 1.85, 0.66))),
+            transform: Transform::from_translation(Vec3::new(projectil.position.x+5.0, projectil.position.y, projectil.position.z)),
             ..default()
         },
         projectil,
